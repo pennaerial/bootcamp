2ac327c3bfb306d3730a80b5b5298e3e0ffea1ca
dhruviyer <iyer.dhruv99@gmail.com>
2017-09-30 15:19:44 -0700

    project finished!

--- src/listener/listener.py
+++ src/listener/listener.py
@@ -1,5 +1,13 @@
+import rospy
+from std_msgs.msg import String
+
+def callback(data):
+    rospy.loginfo(data.data)
+
 def main():
-    pass
+    rospy.init_node('listener', anonymous=True)
+    rospy.Subscriber("chatter", String, callback)
+    rospy.spin()
 
 if __name__ == '__main__':
     main()
--- src/publisher/publisher.py
+++ src/publisher/publisher.py
@@ -1,5 +1,17 @@
-def main():
-    pass
+import rospy
+from std_msgs.msg import String
 
+def main():
+    pub = rospy.Publisher('chatter', String, queue_size=10)
+    rospy.init_node('talker', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    while not rospy.is_shutdown():
+        var = raw_input("Please enter a message: ")
+        pub.publish(var)
+        rate.sleep()
+        
 if __name__ == '__main__':
-    main()
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
